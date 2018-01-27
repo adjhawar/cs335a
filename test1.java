a92aa6a250e7986fad8ad3fcacbd84178dcd4ea7
adjhawar <adjhawar0697@gmail.com>
2018-01-28 02:20:04 +0530

    literal errors added

--- test1.java
+++ test1.java
@@ -1,7 +1,18 @@
-classes adit{
+class aditya{
 	void main(){
-		int a=1;
-		print("Hello world");
-		return ;
+		int chi=1;
+		float pi=3.1425;
+		char ch='A;
+		"aditya
+		switch(ch){
+			case 0:	break;
+			case 1:	break;
+			default: break;
+		}
+		if(pi>=1)
+			print("Huuray");
+		else
+			print("Nothing");
+		return 0;
 	}
 }
