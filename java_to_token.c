e75498856624f92ca0ddc64a9f41339225235536
mraj1609 <rajku@iitk.ac.in>
2018-01-27 23:55:49 +0530

    array finished

--- java_to_token.c
+++ java_to_token.c
@@ -7,10 +7,11 @@ extern char* yytext;
 
 int main(void){
 	int ntoken,vtoken;
-        char *tokens = {CLASS , INSTANCEOF, NEW, SUPER, THIS, BOOL, BYTE ,CHAR, INT , FLOAT, DOUBLE, LONG, SHORT, VOID, BREAK, CASE, DEFAULT, ELSE, IF, SWITCH, CONT, DO, FOR, WHILE, RETURN, CONST, ID, SEP, TRM, COLON, ARRAY_S, ARRAY_E, BLOCK_S, BLOCK_E, PAREN_S, PAREN_E, OPP_ASS, OPP_ADD_ASS, OP_SUB_ASS, OP_DIV_ASS, OP_MUL_ASS, OP_MOD_ASS, OP_LSH_ASS, OP_RSH_ASS, OP_AND_ASS, OP_OR_ASS, OP_XOR_ASS, OP_ZRSH_ASS, OP_CON_AND, OP_CON_OR, OP_OR, OP_XOR, OP_AND, OP_EQ, OP_NEQ, OP_GRE, OP_LES, OP_GEQ, OP_LEQ, OP_RSH, OP_LSH, OP_ADD, OP_SUB, OP_MUL, OP_DIV, OP_MOD, OP_INC, OP_DEC, OP_DOT} 
+        char tokens[80][10] = {"null", "CLASS" , "INSTANCEOF" , "NEW", "SUPER", "THIS", "BOOL", "BYTE" ,"CHAR", "DOUBLE" , "FLOAT", "INT", "LONG", "SHORT", "VOID", "BREAK", "CASE", "DEFAULT", "ELSE", "IF", "SWITCH", "CONT", "DO", "FOR", "WHILE", "RETURN", "CONST", "ID", "SEP", "TRM", "COLON", "ARRAY_S", "ARRAY_E", "BLOCK_S", "BLOCK_E", "PAREN_S", "PAREN_E", "OPP_ASS", "OPP_ADD_ASS", "OP_SUB_ASS", "OP_DIV_ASS", "OP_MUL_ASS", "OP_MOD_ASS", "OP_LSH_ASS", "OP_RSH_ASS", "OP_AND_ASS", "OP_OR_ASS", "OP_XOR_ASS", "OP_ZRSH_ASS", "OP_CON_Q", "OP_CON_AND", "OP_CON_OR", "OP_OR", "OP_XOR", "OP_AND", "OP_EQ", "OP_NEQ", "OP_GRE", "OP_LES", "OP_GEQ", "OP_LEQ", "OP_RSH", "OP_LSH", "OP_ADD", "OP_SUB", "OP_MUL", "OP_DIV", "OP_MOD", "OP_INC", "OP_DEC", "OP_DOT","OP_ZRSH"} ;
 	ntoken=yylex();
 	while(ntoken){
-		printf("%d",ntoken);
+		printf("%s",tokens[ntoken]);
+                
 		ntoken=yylex();
 	}
 	return 0;
