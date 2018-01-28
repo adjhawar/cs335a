ca8d05afec95b3df301b0fd98c912c5b031b8911
mraj1609 <rajku@iitk.ac.in>
2018-01-28 21:26:15 +0530

    print done

--- java_to_token.c
+++ java_to_token.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-#include<stdlib.h>
+#include <stdlib.h>
+#include <string.h>
 #include "java_to_token.h"
 
 extern int yylex();
@@ -9,24 +10,31 @@ struct category {
 		char* lexeme ;
 		struct category* next ;
 	} ;
-void add(struct category *head)
+struct category* add(struct category *head)
 {
+        struct category* temp = head;
 	struct category* curr = (struct category*)malloc(sizeof(struct category));
-	curr->lexeme = yytext;
+        curr->lexeme = (char *)malloc(15*sizeof(char));
+      //  printf("%s hello\n",yytext);
+	strcpy(curr->lexeme,yytext);
 	curr->next=NULL;
 	if(head==NULL)
 	{
 		head=curr;
+                return head;
 	}
 	else
 	{
-		while(head->next!=NULL)
+		while(head->next!=NULL && strcmp(head->lexeme,yytext))
 		{
 			head=head->next;
 		}
-		 head->next=curr;
+		if(strcmp(head->lexeme,yytext))
+		 	head->next=curr;
 	}
+        return temp ;
 }
+
 void Print_lex(struct category *head)
 {
 	printf("%s",head->lexeme);
@@ -37,6 +45,7 @@ void Print_lex(struct category *head)
 	}
 
 }
+
 int main(void){
 
 	int ntoken,vtoken, occur[80]={0};
@@ -45,22 +54,24 @@ int main(void){
 	char tokens[80][15] = {"null", "CLASS" , "INSTANCEOF" , "NEW", "SUPER", "THIS", "BOOL", "BYTE" ,"CHAR", "DOUBLE" , "FLOAT", "INT", "LONG", "SHORT", "VOID", "BREAK", "CASE", "DEFAULT", "ELSE", "IF", "SWITCH", "CONT", "DO", "FOR", "WHILE", "RETURN", "CONST", "ID", "SEP", "TRM", "COLON", "ARRAY_S", "ARRAY_E", "BLOCK_S", "BLOCK_E", "PAREN_S", "PAREN_E", "OPP_ASS", "OPP_ADD_ASS", "OP_SUB_ASS", "OP_DIV_ASS", "OP_MUL_ASS", "OP_MOD_ASS", "OP_LSH_ASS", "OP_RSH_ASS", "OP_AND_ASS", "OP_OR_ASS", "OP_XOR_ASS", "OP_ZRSH_ASS", "OP_CON_Q", "OP_CON_AND", "OP_CON_OR", "OP_OR", "OP_XOR", "OP_AND", "OP_EQ", "OP_NEQ", "OP_GRE", "OP_LES", "OP_GEQ", "OP_LEQ", "OP_RSH", "OP_LSH", "OP_ADD", "OP_SUB", "OP_MUL", "OP_DIV", "OP_MOD", "OP_INC", "OP_DEC", "OP_DOT","OP_ZRSH","INT_LIT_D","INT_LIT_O","INT_LIT_H","FLOAT_LIT","CHAR_LIT","STR_LIT","ERROR"} ;
 
 	char lexeme[80][15] = {"", "class", "instanceof", "new" , "super" , "this", "boolean", "byte", "char", "double", "float", "int", "long", "short","void","break","case","default","else","if","switch","continue","do","for","while","return","final","",",",";",":","[","]","{",
-		"}","(",")","=","+=","-=","/=","*=","%=","<<=",">>=","&=","=","^=",">>>=","?","&&","||","|","^","&","==","=",">","<",">=","<=",">>","<<","+","-","*","/","%","++","--",".",">>>","","","","","","","","" };
+		"}","(",")","=","+=","-=","/=","*=","%=","<<=",">>=","&=","=","^=",">>>=","?","&&","||","|","^","&","==","=",">","<",">=","<=",">>","<<","+","-","*","/","%","++","--",".",">>>","","","","","","",""};
 	ntoken=yylex();
 	while(ntoken){
-		if(lexeme[ntoken]=="")
+		if(lexeme[ntoken][0]=='\0') {
 			switch(ntoken)
 			{
-				case 27 : add(Id); break;
-				case 72 : add(Lit_d);break;
-				case 73 : add(Lit_o);break;
-				case 74 : add(Lit_h); break;
-				case 75 : add(Flo);break;
-				case 76 : add(Cha);break;
-				case 77 : add(Str);break;
+				case 27 : Id=add(Id); break;
+				case 72 : Lit_d=add(Lit_d);break;
+				case 73 : Lit_o=add(Lit_o);break;
+				case 74 : Lit_h=add(Lit_h); break;
+				case 75 : Flo=add(Flo);break;
+				case 76 : Cha=add(Cha);break;
+				case 77 : Str=add(Str);break;
 				default : ;
-			}  
-		occur[ntoken]++ ;
+			}
+			//printf("%s hello\n",yytext);
+                   }
+			occur[ntoken]++ ;
 		ntoken=yylex();
 	}
 	for(int i=1;i<80 ; i++)
@@ -68,9 +79,9 @@ int main(void){
 		if(occur[i]>0)
 		{
 			printf("%s  %d ",tokens[i],occur[i]);
-			if(lexeme[i]=="")
-
+			if(lexeme[i][0]=='\0')
 			{
+                               
 				switch(i)
 				{
 					case 27 : Print_lex(Id); break;
