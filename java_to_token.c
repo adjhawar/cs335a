3fec269cd10528b33fe12def9ced3670f4a88ed9
adjhawar <adjhawar0697@gmail.com>
2018-01-28 22:26:54 +0530

    Add modified. Extra spaces from the print statement removed. Scan and Print statements added. ERR corrected.

--- java_to_token.c
+++ java_to_token.c
@@ -11,16 +11,16 @@ struct category {
 		char* lexeme ;
 		struct category* next ;
 	} ;
-struct category* add(struct category *head)
+struct category* add(struct category *head,int *count)
 {
         struct category* temp = head;
 	struct category* curr = (struct category*)malloc(sizeof(struct category));
         curr->lexeme = (char *)malloc(15*sizeof(char));
-      //  printf("%s hello\n",yytext);
 	strcpy(curr->lexeme,yytext);
 	curr->next=NULL;
 	if(head==NULL){
 		head=curr;
+		(*count)++;
 		return head;
 	}
 	else
@@ -29,8 +29,10 @@ struct category* add(struct category *head)
 		{
 			head=head->next;
 		}
-		if(strcmp(head->lexeme,yytext))
+		if(strcmp(head->lexeme,yytext)){
 		 	head->next=curr;
+			(*count)++;
+		}
 	}
         return temp ;
 }
@@ -54,26 +56,26 @@ int main(void){
 	char tokens[82][15] = {"null", "CLASS" , "INSTANCEOF" , "NEW", "SUPER", "THIS", "BOOL", "BYTE" ,"CHAR", "DOUBLE" , "FLOAT", "INT", "LONG", "SHORT", "VOID", "BREAK", "CASE", "DEFAULT", "ELSE", "IF", "SWITCH", "CONT", "DO", "FOR", "WHILE", "RETURN", "CONST", "ID", "SEP", "TRM", "COLON", "ARRAY_S", "ARRAY_E", "BLOCK_S", "BLOCK_E", "PAREN_S", "PAREN_E", "OPP_ASS", "OPP_ADD_ASS", "OP_SUB_ASS", "OP_DIV_ASS", "OP_MUL_ASS", "OP_MOD_ASS", "OP_LSH_ASS", "OP_RSH_ASS", "OP_AND_ASS", "OP_OR_ASS", "OP_XOR_ASS", "OP_ZRSH_ASS", "OP_CON_Q", "OP_CON_AND", "OP_CON_OR", "OP_OR", "OP_XOR", "OP_AND", "OP_EQ", "OP_NEQ", "OP_GRE", "OP_LES", "OP_GEQ", "OP_LEQ", "OP_RSH", "OP_LSH", "OP_ADD", "OP_SUB", "OP_MUL","OP_DIV","OP_MOD","OP_INC","OP_DEC","OP_DOT","OP_ZRSH","INT_LIT_D","INT_LIT_O","INT_LIT_H","FLOAT_LIT","CHAR_LIT","STR_LIT","ERROR","IGN","PRINT","SCAN"} ;
 
 
-	char lexeme[80][15] = {"", "class", "instanceof", "new" , "super" , "this", "boolean", "byte", "char", "double", "float", "int", "long", "short","void","break","case","default","else","if","switch","continue","do","for","while","return","final","",",",";",":","[","]","{",
-		"}","(",")","=","+=","-=","/=","*=","%=","<<=",">>=","&=","=","^=",">>>=","?","&&","||","|","^","&","==","=",">","<",">=","<=",">>","<<","+","-","*","/","%","++","--",".",">>>","","","","","","",""};
+	char lexeme[82][15] = {"","class", "instanceof", "new" , "super" , "this", "boolean", "byte", "char", "double","float","int","long","short","void","break","case","default","else","if","switch","continue","do","for","while","return","final","",",",";",":","[","]","{","}","(",")","=","+=","-=","/=","*=","%=","<<=",">>=","&=","=","^=",">>>=","?","&&","||","|","^","&","==","=",">","<",">=","<=",">>","<<","+","-","*","/","%","++","--",".",">>>","","","","","","","","ign","print","scan"};
 	ntoken=yylex();
 	while(ntoken){
 		if(lexeme[ntoken][0]=='\0') {
 			switch(ntoken)
 			{
-				case 27 : Id=add(Id); break;
-				case 72 : Lit_d=add(Lit_d);break;
-				case 73 : Lit_o=add(Lit_o);break;
-				case 74 : Lit_h=add(Lit_h); break;
-				case 75 : Flo=add(Flo);break;
-				case 76 : Cha=add(Cha);break;
-				case 77 : Str=add(Str);break;
+				case 27 : Id=add(Id,&occur[ntoken]); break;
+				case 72 : Lit_d=add(Lit_d,&occur[ntoken]);break;
+				case 73 : Lit_o=add(Lit_o,&occur[ntoken]);break;
+				case 74 : Lit_h=add(Lit_h,&occur[ntoken]); break;
+				case 75 : Flo=add(Flo,&occur[ntoken]);break;
+				case 76 : Cha=add(Cha,&occur[ntoken]);break;
+				case 77 : Str=add(Str,&occur[ntoken]);break;
+				case 78 : Err=add(Err,&occur[ntoken]);break;
 				default : ;
 			}
 			//printf("%s hello\n",yytext);
                    }
-			occur[ntoken]++ ;
-
+		else
+			occur[ntoken]++;
 		ntoken=yylex();
 	}
 	for(int i=1;i<82 ; i++)
@@ -93,7 +95,7 @@ int main(void){
 					case 75 : Print_lex(Flo);break;
 					case 76 : Print_lex(Cha);break;
 					case 77 : Print_lex(Str);break;
-					case 80	: Print_lex(Err);break;
+					case 78	: Print_lex(Err);break;
 					default : ;
 				}
 			}
