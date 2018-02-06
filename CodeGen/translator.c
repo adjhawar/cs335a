b3926d095beded598c4981cfe3717a5619c5fc48
mraj1609 <rajku@iitk.ac.in>
2018-02-06 21:51:21 +0530

    Update translator.c

--- CodeGen/translator.c
+++ CodeGen/translator.c
@@ -1,6 +1,7 @@
-#include "iostream.h"
 #include "stdio.h"
+#include "stdlib.h"
 enum InstrType{ Assignment , Function ,Jump , Pointer , Indexed_Ass};
+enum Operator{ }
 
 //Data structure to hold 3ac instruction
 typedef struct {
@@ -13,10 +14,10 @@ Operator op
 } Instruction3AC;
 
 //Data structure to hold symbol table
+
 FILE *fptr ;
 if(fptr = fopen("../IL_Program.txt","r")==NULL)
 {
 printf("Error in opening IL_Program.txt . Aborting....");
 exit(0);
 }
-
