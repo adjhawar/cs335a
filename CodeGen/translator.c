152d1d3928c0cbb039bd652b20ca208e716ef02b
hpanthi <h.p.hariompanthi@gmail.com>
2018-02-09 10:50:14 +0530

    Update translator.c

--- CodeGen/translator.c
+++ CodeGen/translator.c
@@ -1,7 +1,8 @@
-#include "stdio.h"
-#include "stdlib.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 enum InstrType{ Assignment , Function ,Jump , Pointer , Indexed_Ass};
-enum Operator{ }
+enum TACkeywords{+ , - , * , / , % , > , < , >= , <= , != , == , = , ifgoto , goto , ret , call , label , print , scan }
 
 //Data structure to hold 3ac instruction
 typedef struct {
@@ -10,14 +11,39 @@ SymtabEntry *in1;
 SymtabEntry *in2
 SymtabEntry *out;
 int target; // jump target
-Operator op
+TACkeywords op
 } Instruction3AC;
 
-//Data structure to hold symbol table
+//To store IL code 
+Instruction3AC *ir;
 
-FILE *fptr ;
-if(fptr = fopen("../IL_Program.txt","r")==NULL)
-{
-printf("Error in opening IL_Program.txt . Aborting....");
-exit(0);
+//Data structure to hold symbol table
+typedef struct {
+char label[100],type[10];
+struct SymbtabEntry *next;
+}SymbtabEntry;
+
+FILE *fptr = fopen("IL_Program.csv","r");
+if(fptr==NULL){
+  printf("Error in opening IL_Program.csv . Aborting....");
+  exit(0);
+}
+else {
+  int nline = 0;
+  char line[1024];
+  while (fgets(line, 1024, fptr)){
+    *(ir + nline) = malloc(sizeof(Instruction3AC));
+    char *token;
+    char *rest = line;
+    char strs[6][100];
+    int i = 0;
+    while ((token = strtok_r(rest, ",", &rest))){
+      strcpy(strs[i], token);
+      i++;
+  }
+    nline = atoi(strs[0]);
+    char *key = strs[1];
+    if (key == '='){
+      
+    }
 }
