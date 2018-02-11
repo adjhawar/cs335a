c067c5f477f3f909c0ee49c4d8238a0e5addba8d
mraj1609 <rajku@iitk.ac.in>
2018-02-12 03:53:36 +0530

    code gen and get reg started

--- CodeGen/code_gen.c
+++ CodeGen/code_gen.c
@@ -29,9 +29,8 @@ for(int i=0;i<nline;i++)
           case mul : printf("imul %s,%s\n",register[R_y],register[R_z]);
                      break;
           case divi : printf("idiv %s\n",register[R_z]);
-                      
                      break; 
-          case mod : printf("add %s,%s\n",register[R_y],register[R_z]);
+          case mod : printf("idiv %s\n",register[R_z]);
                      break; 
 
 
@@ -40,7 +39,7 @@ for(int i=0;i<nline;i++)
         
         }
         
-        printf("mov %s,%s\n",register[R_x],register[R_z]);     
+        printf("mov %s,%s\n",register[R_x],register[R_y]);     
       }
   
   
@@ -49,6 +48,69 @@ for(int i=0;i<nline;i++)
 
 
 }
+  
+  void getReg(i)
+  {
+    int r;
+  if(ir[i].typ==Assignment)
+  {
+    if(ir[i].op==add || ir[i].op==sub )
+    {
+       if(ir[i]->in1->add_des.reg_no==-1)      // -1 denotes that in1's is not stored in register
+       {
+           if(ir[i]->in2->add_des.reg_no==-1)
+           {
+                r=empty_reg() ; //this function returns a empty register
+                printf("mov %s,[%s]",register[r],ir[i]->in1->lexeme);
+                ir[i]->in1->add_des.reg_no=r;
+                reg_des[r]=ir[i]->in1;
+           }
+           else
+           {
+             printf("add [%s],%s",ir[i]->in1->lexeme,register[ir[i]->in2->add_des.reg_no]);
+             //to do : update address and register descriptor
+           }
+         
+         
+       }
+    
+    
+    
+    
+    
+    }
+  
+  
+  
+  
+  
+  }
+ int empty_reg()
+ {
+   for(int i=0;i<6;i++)
+   {
+     if(reg_des[i]!=NULL)
+     {
+       return i;
+     }
+   }
+   for(int i=0;i<6;i++)
+   {
+     if(reg_des[i]->add_des->mem)
+     {
+       return i;
+     }
+   }
+   printf("mov [%s],%s",reg_des[1]->lexexe,register[reg_des[1]->add_des.reg_no]) // empty the register no 1 
+   return 1;
+ }
+  
+  
+  
+  
+  
+  }
+  
 
 
 
