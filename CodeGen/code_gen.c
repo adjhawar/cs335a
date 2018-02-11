6de808b4d78bce0f68d3628fa9f96f3b74f7acd1
mraj1609 <rajku@iitk.ac.in>
2018-02-11 17:15:31 +0530

    Update code_gen.c

--- CodeGen/code_gen.c
+++ CodeGen/code_gen.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<global.h>
 char register[6][4] = { "EAX","EBX","ECX","EDX","ESI","EDI"};
 int R_x,R_y,R_z;
 void main()
@@ -8,12 +9,38 @@ for(int i=0;i<nline;i++)
 {
   if(ir[i].typ==Assignment)
   {
-      if(ir[i].op==add)
+      if(ir[i].op!=assgn)
       {
         getReg(i);
-        if()
-      
-      
+        if(strcmp(reg_des[R_y]->lexeme,ir[i].in1->lexeme))
+        {
+          printf("mov %s,[%s]\n",register[R_y],ir[i].in1->lexeme);
+        }
+        if(strcmp(reg_des[R_z]->lexeme,ir[i].in2->lexeme))
+        {
+          printf("mov %s,[%s]\n",register[R_z],ir[i].in2->lexeme);
+        }
+        switch(op)
+        {
+          case add : printf("add %s,%s\n",register[R_y],register[R_z]);
+                     break; 
+          case sub : printf("sub %s,%s\n",register[R_y],register[R_z]);
+                     break; 
+          case mul : printf("imul %s,%s\n",register[R_y],register[R_z]);
+                     break;
+          case divi : printf("idiv %s\n",register[R_z]);
+                      
+                     break; 
+          case mod : printf("add %s,%s\n",register[R_y],register[R_z]);
+                     break; 
+
+
+        
+        
+        
+        }
+        
+        printf("mov %s,%s\n",register[R_x],register[R_z]);     
       }
   
   
