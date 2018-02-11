7e8e39a7b907373e6e70fc6ee0a9f7481f3cbdd2
mraj1609 <rajku@iitk.ac.in>
2018-02-11 15:57:19 +0530

    code gen begins

---
+++ CodeGen/code_gen.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<stdlib.h>
+char register[6][4] = { "EAX","EBX","ECX","EDX","ESI","EDI"};
+int R_x,R_y,R_z;
+void main()
+{
+for(int i=0;i<nline;i++)
+{
+  if(ir[i].typ==Assignment)
+  {
+      if(ir[i].op==add)
+      {
+        getReg(i);
+        if()
+      
+      
+      }
+  
+  
+  }
+
+
+
+}
+
+
+
+
+
+
+
+}
