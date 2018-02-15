31b4588147252eefb4b9b733160f8666057431ac
adjhawar <adjhawar0697@gmail.com>
2018-02-15 10:14:59 +0530

    Grant me access

--- CodeGen/test.s
+++ CodeGen/test.s
@@ -1,27 +1,29 @@
 .data
+i:  .quad  0
 a:  .quad  0
-b:  .quad  0
 c:  .quad  0
 str:   .string "%d \n"
 str1:   .string "%d"
  .text
  .globl main 
  main:
-movq $100,a
-	 movq a,%rax
-	 shlq $2,%rax
-	 movq %rax,b
-	 movq a,%rax
-	 shrq $2,%rax
-	 movq %rax,c
+movq $0,i
+	 movq $10,%rax
+	 andq $15,%rax
+	 movq %rax,a
+movq %rax,a
 movq $str,%rdi
- movq b,%rsi
+ movq a,%rsi
  movq $0,%rax
 pushq %r10 
  pushq %r11 
  call printf 
  popq %r11 
  popq %r10 
+	 movq $10,%rax
+	 orq $15,%rax
+	 movq %rax,c
+movq %rax,c
 movq $str,%rdi
  movq c,%rsi
  movq $0,%rax
